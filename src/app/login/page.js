06be5ce2091c2cf36a104bbc663aed3786cafce3
RubenMkda <Moncadaruben22@gmail.com>
2023-08-16 09:01:40 -0400

    fixing login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -15,7 +15,7 @@ const LoginUser = () => {
                     </form>
                 </main>
                 <footer className="flex flex-col items-center justify-center">
-                    <p>You don't have an account?</p>
+                    <p>You don`&apos;`t have an account?</p>
                     <Link className="group transition duration-300 text-xl text-green-500" href='/signup'>
                         Register
                          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-green-500"></span>
